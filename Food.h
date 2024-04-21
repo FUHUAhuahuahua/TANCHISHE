e5378dba54a0c9fa9f42819ad89e6693478b4f55
AOLIAO12312 <143392217+AOLIAO12312@users.noreply.github.com>
2024-04-21 11:53:07 +0800

    Update Food.h

--- Food.h
+++ Food.h
@@ -4,6 +4,8 @@ enum{SMALL,MIDDLE,BIG};
 class Food :public Object {
 public:
 	Food(int size,int color, int posx, int posy);
+	int getColor();
+	int getSize();
 private:
 	int m_Color;
 	int m_Size;

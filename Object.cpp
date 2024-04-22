297da443817d1c789074ca88e45a7031a01d38db
AOLIAO12312 <143392217+AOLIAO12312@users.noreply.github.com>
2024-04-22 08:54:29 +0800

    Update Object.cpp

--- Object.cpp
+++ Object.cpp
@@ -1,10 +1,4 @@
 #include"Object.h"
-void Object::AddNode(int x, int y) {//Ò»¸öx¶ÔÓ¦Ò»¸öy
-	assert(x >= 0 && x < COL);
-	assert(y >= 0 && y < ROW);
-	m_Position[0].push_front(x);
-	m_Position[1].push_front(y);
-	++m_Length;#include"Object.h"
 void Object::AddNode(int x, int y) {//一个x对应一个y
 	assert(x >= 0 && x < COL);
 	assert(y >= 0 && y < ROW);
@@ -13,16 +7,12 @@ void Object::AddNode(int x, int y) {//一个x对应一个y
 	++m_Length;
 }
 
-pair<int, int> Object::getPositon(int x) {
-	list<int>::const_iterator itx = m_Position[0].begin();
-	list<int>::const_iterator ity = m_Position[1].begin();
-	for (int i = 0; i < x; i++) {
-		itx++, ity++;
-	}
-	return pair<int, int>(*itx, *ity);
+pair<int, int> Object::getPositon(int Seq) {
+	assert(Seq < m_Length);
+	return pair<int, int>(m_Position[0][Seq], m_Position[1][Seq]);
 }
 
 int Object::getLength() {
 	return this->m_Length;
 }
-}
+

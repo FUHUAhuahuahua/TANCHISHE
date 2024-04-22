492c1443a4dd2cb3bb85bb3aa454b001b0fdb5a3
AOLIAO12312 <143392217+AOLIAO12312@users.noreply.github.com>
2024-04-22 08:25:34 +0800

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -4,10 +4,11 @@ void Snake::DeleteBackNode() {
 	m_Position[1].pop_back();
 	--m_Length;
 }
+
 Snake::Snake(int Orientation, int Length, int beginx, int beginy) {
-	m_Length = 0;
-	list<int>x;
-	list<int>y;
+	m_Length = Length;
+	deque<int>x;
+	deque<int>y;
 	if (Orientation == UP) {
 		assert(beginy - Length >= 0);
 		for (int i = Length - 1; i >= 0 ; --i) {
@@ -40,59 +41,86 @@ Snake::Snake(int Orientation, int Length, int beginx, int beginy) {
 	m_Position.push_back(y);
 	m_Orientation = Orientation;
 }
-int Snake::getLenth() {
-	return m_Length;
-}
-void Snake::PrintPositon() {
-	list<int>::const_iterator itx = m_Position[0].begin();
-	list<int>::const_iterator ity = m_Position[1].begin();
-	for (;itx != m_Position[0].end() && ity != m_Position[1].end(); itx++, ity++)//遍历坐标	打印坐标
-		cout << *itx << "," << *ity << endl;
-}
+
+//void Snake::PrintPositon() {
+//	deque<int>::const_iterator itx = m_Position[0].begin();
+//	deque<int>::const_iterator ity = m_Position[1].begin();
+//	for (;itx != m_Position[0].end() && ity != m_Position[1].end(); itx++, ity++)//遍历坐标	打印坐标
+//		cout << *itx << "," << *ity << endl;
+//}
+
 void Snake::Move() {
 	DeleteBackNode();
 	switch (m_Orientation)
 	{
 	case UP:
-		AddNode(*(m_Position[0].begin()), *(m_Position[1].begin()) - 1);
+		if (*(m_Position[1].begin()) == 0) {
+			AddNode(*(m_Position[0].begin()), ROW - 1);
+		}
+		else {
+			AddNode(*(m_Position[0].begin()), *(m_Position[1].begin()) - 1);
+		}
 		break;
 	case DOWN:
-		AddNode(*(m_Position[0].begin()), *(m_Position[1].begin()) + 1);
+		if (*(m_Position[1].begin()) == ROW - 1) {
+			AddNode(*(m_Position[0].begin()), 0);
+		}
+		else {
+			AddNode(*(m_Position[0].begin()), *(m_Position[1].begin()) + 1);
+		}
 		break;
 	case LEFT:
-		AddNode(*(m_Position[0].begin()) - 1, *(m_Position[1].begin()));
+		if (*(m_Position[0].begin()) == 0) {
+			AddNode(COL - 1, *(m_Position[1].begin()));
+		}
+		else {
+			AddNode(*(m_Position[0].begin()) - 1, *(m_Position[1].begin()));
+		}
 		break;
 	case RIGHT:
-		AddNode(*(m_Position[0].begin()) + 1, *(m_Position[1].begin()));
-		break;
-	}
-}
-void Snake::ChangeOrientation(int orientation) {
-	switch (orientation) {
-	case UP:case DOWN:case LEFT:case RIGHT:
-		m_Orientation = orientation;
-		break;
-	default:
-		assert(orientation <= RIGHT && orientation >= UP);
-		break;
-	}
-}
-void Snake::ShowSnakeInMap() {
-	for (int i = 0; i < ROW; ++i) {
-		for (int j = 0; j < COL; ++j) {
-			if (SnakeOccupied(j, i)) {
-				cout << "#";
-			}
-			else {
-				cout << "-";
-			}
+		if (*(m_Position[0].begin()) == COL-1) {
+			AddNode(0, *(m_Position[1].begin()));
 		}
-		cout  << i << endl;
+		else {
+			AddNode(*(m_Position[0].begin()) + 1, *(m_Position[1].begin()));
+		}
+		break;
 	}
 }
+
+void Snake::ChangeOrientation(int orientation) {
+	assert(orientation <= RIGHT && orientation >= UP);
+	if (orientation == UP && this->m_Orientation != DOWN) {
+		m_Orientation = UP;
+	}
+	else if (orientation == DOWN && this->m_Orientation != UP) {
+		m_Orientation = DOWN;
+	}
+	else if (orientation == LEFT && this->m_Orientation != RIGHT) {
+		m_Orientation = LEFT;
+	}
+	else if (orientation == RIGHT && this->m_Orientation != LEFT) {
+		m_Orientation = RIGHT;
+	}
+}
+
+//void Snake::ShowSnakeInMap() {
+//	for (int i = 0; i < ROW; ++i) {
+//		for (int j = 0; j < COL; ++j) {
+//			if (SnakeOccupied(j, i)) {
+//				cout << "#";
+//			}
+//			else {
+//				cout << "-";
+//			}
+//		}
+//		cout  << i << endl;
+//	}
+//}
+
 bool Snake::SnakeOccupied(int x, int y) {
-	list<int>::const_iterator itx = m_Position[0].begin();
-	list<int>::const_iterator ity = m_Position[1].begin();
+	deque<int>::const_iterator itx = m_Position[0].begin();
+	deque<int>::const_iterator ity = m_Position[1].begin();
 	for (; itx != m_Position[0].end() && ity != m_Position[1].end(); itx++, ity++) {
 		if (x == *itx && y == *ity) {
 			return true;
@@ -100,11 +128,19 @@ bool Snake::SnakeOccupied(int x, int y) {
 	}
 	return false;
 }
-pair<int, int> Snake::getPositon(int x) {
-	list<int>::const_iterator itx = m_Position[0].begin();
-	list<int>::const_iterator ity = m_Position[1].begin();
-	for (int i = 0; i < x; i++) {
-		itx++, ity++;
+
+void Snake::DrawSnakeBody(int x,int y) {
+	fillcircle(x * CELL_SIZE + CELL_SIZE / 2, y * CELL_SIZE + CELL_SIZE / 2, Snake_Body_Size);
+}
+
+void Snake::DrawSnake() {
+	setfillcolor(MIDDLEPURPLE);
+	for (int i = 0; i < m_Length; i++) {
+		pair<int, int>p;
+		p = this->getPositon(i);
+		DrawSnakeBody(p.first, p.second);
+		if (!i) {
+			setfillcolor(LIGHTPURPLE);
+		}
 	}
-	return pair<int,int>(*itx,*ity);
 }

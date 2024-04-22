5398c4ea7e81ea9a895af14c72177ec0af44afba
AOLIAO12312 <143392217+AOLIAO12312@users.noreply.github.com>
2024-04-22 08:42:46 +0800

    Update Wall.h

--- Wall.h
+++ Wall.h
@@ -1,8 +1,10 @@
 #pragma once
 #include"Object.h"
+#include"Snake.h"
 class Wall : public Object{
 public:
 	Wall(int Color, int Orientation, int Length, int x, int y);
+	bool BeHit(Snake& s);//如果被被蛇头撞上返回true	否则返回false
 	int getColor();
 private:
 	int m_Color;

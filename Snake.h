23234b40e84537b01bfb3bbadc050820c7065b5b
AOLIAO12312 <143392217+AOLIAO12312@users.noreply.github.com>
2024-04-22 08:41:39 +0800

    Update Snake.h

--- Snake.h
+++ Snake.h
@@ -8,6 +8,7 @@ public:
 	void ChangeOrientation(int orientation);//改变蛇前进朝向
 	bool SnakeOccupied(int x, int y);//输入xy坐标，判断该坐标是否被蛇所占用，是则返回true，否则返回false
 	void DrawSnake();
+	bool BeHit(Snake& s);//如果被被蛇头撞上返回true	否则返回false
 	static void DrawSnakeBody(int x,int y);
 	//void ShowSnakeInMap();//以地图的形式打印出蛇的位置
 	//void PrintPositon();//以坐标形式打印蛇的位置

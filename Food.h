271e3b9fe96e642b922e884b7f1af4c5b7e5b82d
AOLIAO12312 <143392217+AOLIAO12312@users.noreply.github.com>
2024-04-22 08:42:07 +0800

    Update Food.h

--- Food.h
+++ Food.h
@@ -1,9 +1,11 @@
 #pragma once
 #include"Object.h"
+#include"Snake.h"
 enum{SMALL,MIDDLE,BIG};
 class Food :public Object {
 public:
 	Food(int size,int color, int posx, int posy);
+	bool BeHit(Snake& s);//如果被被蛇头撞上返回true	否则返回false
 	int getColor();
 	int getSize();
 private:

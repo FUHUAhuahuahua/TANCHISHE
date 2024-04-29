05f02c188c938515b0207c48fa656c688d10044d
FUHUAhuahuahua <162676455+FUHUAhuahuahua@users.noreply.github.com>
2024-04-29 08:52:22 +0800

    Update Food.h

--- Food.h
+++ Food.h
@@ -1,13 +1,14 @@
 #pragma once
 #include"Object.h"
 #include"Snake.h"
-enum{SMALL,MIDDLE,BIG};
+enum { SMALL, MIDDLE, BIG };
 class Food :public Object {
 public:
-	Food(int size,int color, int posx, int posy);
+	Food(int size, int color, int posx, int posy);
 	bool BeHit(Snake& s);//如果被被蛇头撞上返回true	否则返回false
 	int getColor();
 	int getSize();
+	void DrawFood(int it_size); // 绘制食物
 private:
 	int m_Color;
 	int m_Size;
